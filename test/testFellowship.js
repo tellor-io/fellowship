7efa670a8ef282bb052ca3a1f4a0895a460d23ab
themandalore <fett.nicholas@yahoo.com>
2021-01-04 15:45:54 -0500

    tests

--- test/testFellowship.js
+++ test/testFellowship.js
@@ -1,7 +1,7 @@
 const helpers = require("./helpers/test_helpers.js");
-const Fellowship = require("Fellowship.sol");
-const Rivendale = require("Rivendale.sol")
-const ERC20 = require("/testContracts/ERC20.sol")
+const Fellowship = artifacts.require("Fellowship.sol");
+const Rivendale = artifacts.require("Rivendale.sol")
+const ERC20 = artifacts.require("/testContracts/ERC20.sol")
 
 contract("Fellowship Tests", function(accounts) {
   let fellowship;
@@ -9,9 +9,9 @@ contract("Fellowship Tests", function(accounts) {
   let token;
 
   beforeEach("Setup contract for each test", async function() {
-    token - await ERC20.new();
+    token - await ERC20.new("Test","TEST");
     for(i=0;i<5;i++){
-        await token.faucet({from:accounts[i]})
+        await token.faucet(accounts[i],{from:accounts[i]})
     }
     fellowship = await Fellowship.new(token.address);
     rivendale = await Rivendale.new(fellowship.address);
