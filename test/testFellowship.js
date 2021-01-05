4b11d9ca829a267e3b664bd89e5d5ca6b93ebdba
themandalore <fett.nicholas@yahoo.com>
2021-01-05 15:07:38 -0500

    setting up tests

--- test/testFellowship.js
+++ test/testFellowship.js
@@ -9,16 +9,64 @@ contract("Fellowship Tests", function(accounts) {
   let token;
 
   beforeEach("Setup contract for each test", async function() {
-    token - await ERC20.new("Test","TEST");
+    token = await ERC20.new("Test","TEST");
     for(i=0;i<5;i++){
         await token.faucet(accounts[i],{from:accounts[i]})
     }
     fellowship = await Fellowship.new(token.address);
     rivendale = await Rivendale.new(fellowship.address);
-    await fellowship.newRivendale(rivendale.address);
+  });
+
+  it("Test New Walker", async function() {
+    await fellowship.newWalker(accounts[1],"Frodo")
+    assert(1==1)
   });
 
   it("Test Deposit Stake", async function() {
     assert(1==1)
   });
+
+  it("Test Banish Walker", async function() {
+    assert(1==1)
+  });
+
+  it("Test Set Walker information", async function() {
+    await fellowship.newWalker(accounts[1],"Frodo")
+    assert(1==1)
+  });  
+  it("Test Getters", async function() {
+    //checks whether they are a Walker
+// function isWalker(address _a) public view returns(bool _i){
+//   function getWalkerDetails(address _walker) external view returns(uint,uint,string memory,uint){
+//   function getWalkerInformation(address _walker, bytes32 _input) external view returns(bytes memory _output){
+  
+  });  
+
+  it("Test Set Stake Amount", async function() {
+    assert(1==1)
+  });  
+
+  it("Test Set Fellowship Size", async function() {
+    assert(1==1)
+  });  
+
+  it("Test New Rivendale", async function() {
+    await fellowship.newRivendale(rivendale.address);
+    assert(fellowship.rivendale.call() == rivendale.address, "rivendale address should be correct")
+    await helper.expectThrow(fellowship.newWalker(accounts[1], "fake walker"));
+  });  
+  it("Test Slash Walker", async function() {
+    assert(1==1)
+  });  
+
+  it("Test Deposit Payment / Recieve Reward", async function() {
+    assert(1==1)
+  });  
+
+  it("Test calculate reward", async function() {
+    assert(1==1)
+  });  
+  it("Test Staking Withdraw / Request", async function() {
+    assert(1==1)
+  });  
 });
