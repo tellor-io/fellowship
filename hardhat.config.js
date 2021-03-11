751910111f7df90432f0725270b5ab37cad4e8fd
Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>
2021-03-11 13:02:18 +0200

    added main and test networks
    Signed-off-by: Krasi Georgiev <8903888+krasi-georgiev@users.noreply.github.com>

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,8 +9,16 @@ module.exports = {
   networks: {
     hardhat: {
     },
+    test: {
+      url: `${process.env.NORE_URL_TEST}`,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    main: {
+      url: `${process.env.NORE_URL_MAIN}`,
+      accounts: [process.env.PRIVATE_KEY]
+    },
     maticTest: {
-      url: "https://rpc-mumbai.maticvigil.com/",
+      url: "https://rpc-mumbai.maticvigil.com",
       accounts: [process.env.PRIVATE_KEY]
     },
     maticMain: {
@@ -24,7 +32,7 @@ module.exports = {
       accounts: [process.env.PRIVATE_KEY]
     },
     bscMain: {
-      url: "https://bsc-dataseed.binance.org/",
+      url: "https://bsc-dataseed1.binance.org:443",
       chainId: 56,
       gasPrice: 20000000000,
       accounts: [process.env.PRIVATE_KEY]
